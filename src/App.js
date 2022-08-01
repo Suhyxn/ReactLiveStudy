761531c517ac134721e826a095f5beb31e364679
Suhyxn <momentomori21489@gmail.com>
2022-08-01 21:20:26 +0900

    React UseRef 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Login from './components/Login'
+import Refs from './components/Refs'
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      <Refs/>
     </div>
   );
 }
---
+++ src/components/Refs.js
@@ -0,0 +1,20 @@
+import React, {useRef} from 'react'
+
+function Refs() {
+  const inputRef = useRef()
+
+  const keyHandler = (e) => {
+    if (e.key === 'Enter') {
+    console.log(inputRef.current.value)
+    }
+  }
+
+  return (
+    <div>
+      <input ref={inputRef} onKeyDown={keyHandler}/>
+      {/* getElementById 와 같은 느낌 */}
+    </div>
+  )
+}
+
+export default Refs
