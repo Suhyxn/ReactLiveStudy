90021a3ca381d2a8ce840a19bb10f23343e1f830
Suhyxn <momentomori21489@gmail.com>
2022-08-02 19:06:44 +0900

    React styled-components keyframes 활용하기

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+  from {
+    opacity:  0
+  }
+  to {
+    opacity: 1
+  }
+`
 
 export const Button = styled.button`
   outline: none;
@@ -17,4 +26,8 @@ export const Button = styled.button`
     opacity: 0.5;
   }
   cursor: pointer;
+
+  animation-name: ${fadeIn};
+  animation-duration: 1s;
+  animation-timing-function: ease-out;
 `
