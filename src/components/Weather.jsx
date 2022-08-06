cf0ebfb1ce9b44d4ba7cd72117d2a2833ae8f740
Suhyxn <momentomori21489@gmail.com>
2022-08-07 01:48:32 +0900

    React Axios 더 활용해보기

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,45 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useReducer, useState } from 'react'
-import { weatherReducer } from '../reducers/weatherReducer'
+import useAsync from './hooks/useAsync'
+import getWeather from './utils/getWeather'
 
 function Weather() {
-  const queryParams = {
-    serviceKey: process.env.REACT_APP_SERVICE_KEY,
-    pageNo: 1,
-    numOfRows: 100,
-    dataType: 'JSON',
-    base_date: '20220805',
-    base_time: '0500',
-    nx: 55,
-    ny: 127,
-  }
-
-  const [state, dispatch] = useReducer(weatherReducer, {
-    loading: false,
-    data: null,
-    error: null,
-  })
-
-  const fetchData = async () => {
-    dispatch({ type: 'LOADING' })
-    try {
-      const response = await axios.get(
-        '1360000/VilageFcstInfoService_2.0/getVilageFcst',
-        { params: queryParams },
-      )
-      dispatch({
-        type: 'SUCCESS',
-        data: response.data['response']['body']['items']['item'],
-      })
-    } catch (e) {
-      dispatch({ type: 'ERROR', error: e })
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+  const state = useAsync(getWeather)
   const { loading, data: weather, error } = state
 
   if (loading) return <p>로딩중</p>
